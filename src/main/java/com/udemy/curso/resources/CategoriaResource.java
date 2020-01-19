a039fa2349a8d499b416707c8d55d0885a220d8c
Felipe Barros <felipe92barros@gmail.com>
2020-01-19 10:08:54 -0300

    Adicionando Post de categoria

--- src/main/java/com/udemy/curso/resources/CategoriaResource.java
+++ src/main/java/com/udemy/curso/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.udemy.curso.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.curso.domain.Categoria;
 import com.udemy.curso.services.CategoriaService;
@@ -24,5 +29,16 @@ public class CategoriaResource {
 		Categoria obj = service.buscarPorId(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> inserirCategoria(@RequestBody Categoria obj){
+		
+		obj = service.inserirCategoria(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
